94896e17c49bbb6dcda6e0126bf15c6578e93e57
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2024-09-02 23:51:12 +0530

    Fixed player if queue is empty

--- src/components/layout/player/index.tsx
+++ src/components/layout/player/index.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import React from "react";
 import PlayerTrack from "./track";
 import PlayerName from "./name";
 import PlayerControls from "./controls";
 import PlayerOptions from "./options";
+import usePlayer from "@/contexts/player";
 
 export default function Player() {
+    const player = usePlayer();
+
+    if (player.queue.length === 0) {
+        return null;
+    }
+
     return (
         <footer className="relative h-20" id="fullscreen">
             <PlayerTrack />
