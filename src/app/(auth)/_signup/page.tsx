add38f9dc50c7d89a6dc0e3c5ced9a7dc0350d7e
Shivam Dewangan <110464857+shivamdevs@users.noreply.github.com>
2024-09-17 23:09:13 +0530

    Update: fixed eslint error

--- src/app/(auth)/_signup/page.tsx
+++ src/app/(auth)/_signup/page.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import SignupPage from "./_signup";
-import { SupabaseServerUser } from "@/supabase/server";
-import { PageProps } from "@/types/args";
-import { redirect } from "next/navigation";
+// import React from "react";
+// import SignupPage from "./_signup";
+// import { SupabaseServerUser } from "@/supabase/server";
+// import { PageProps } from "@/types/args";
+// import { redirect } from "next/navigation";
 
-export default async function Page({ searchParams }: PageProps) {
-    const user = await SupabaseServerUser();
-    const continuePath = (searchParams?.continue as string) || "/";
+// export default async function Page({ searchParams }: PageProps) {
+//     const user = await SupabaseServerUser();
+//     const continuePath = (searchParams?.continue as string) || "/";
 
-    if (user) {
-        return redirect(continuePath);
-    }
+//     if (user) {
+//         return redirect(continuePath);
+//     }
 
-    return <SignupPage />;
-}
+//     return <SignupPage />;
+// }
